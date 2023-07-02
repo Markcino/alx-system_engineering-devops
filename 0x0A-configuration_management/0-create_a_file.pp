a6864c33b9b6ff8d0e87e516c25300357a546fe3
Markcino <33788083+Markcino@users.noreply.github.com>
2023-07-02 07:40:05 +0000

    0x0B-ssh

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# A resource declaration
-file { '/tmp/codingschool':
-  ensure  => file,
-  path    => '/tmp/codingschool',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+#Create a file in /tmp
+
+file { 'school':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
